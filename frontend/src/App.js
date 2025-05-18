6223f3e5dced846803c11388f0209dffbf3327d2
Diego Barajas (devsau) <81001205+DiegoBarajas@users.noreply.github.com>
2025-05-18 10:32:45 -0600

    Uploaded new frontend folder

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import react from "react";
+import { Route, Routes } from "react-router-dom";
+
+import Index from "./pages/Index";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={ <Index/> } />
+    </Routes>
+  )
+}
+
+export default App;
